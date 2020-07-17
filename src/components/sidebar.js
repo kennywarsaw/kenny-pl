78a0b9d754eb6724089fb9c3fa27382f8be194a9
Kamil Hajduk <khajduk5@gmail.com>
2020-07-17 13:01:21 +0200

    Test

--- src/components/sidebar.js
+++
@@ -1,8 +0,0 @@
-import React from 'React';
-import styled from 'styled-components';
-
-const Sidebar = () => (
-    <h1>test</h1>
-)
-
-export default Sidebar;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Notes from 'pages/notes';
-
-ReactDOM.render(<Notes />, document.getElementById('root'));
