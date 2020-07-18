f61d844fd8df8f1ba7ac4a95a90cb2c83c67147f
Kamil Hajduk <khajduk5@gmail.com>
2020-07-18 13:48:57 +0200

    Added react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-styled-components`,
+        `gatsby-plugin-react-helmet`,
         `gatsby-plugin-zeit-now`,
         {
             resolve: 'gatsby-plugin-web-font-loader',
